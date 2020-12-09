9664f89182ebc9d0c7d213fea6cc3f74171239bb
Christin Banerjee <Christinprice5@gmail.com>
2020-12-09 08:24:40 -0500

    wired up josn working

--- src/main/java/com/dyvrspages3/demo/WebConfig.java
+++ src/main/java/com/dyvrspages3/demo/WebConfig.java
@@ -1,16 +1,16 @@
-package com.dyvrspages3.demo;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PATCH", "DELETE");
-    }
-
-}
+//package com.dyvrspages3.demo;
+//Don't need in this project. Use this when I want others to use my information as an outside Json resource.
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedMethods("GET", "POST", "PATCH", "DELETE");
+//    }
+//
+//}
--- src/main/resources/static/js/fetch-api.js
+++ src/main/resources/static/js/fetch-api.js
@@ -1,37 +1,37 @@
 export { fetchCategory, fetchStore, fetchTeam, fetchSpotlight, fetchAllCategories, fetchCat1, addNewStore, updateStore };
 const fetchCategory = async (id) => {
   return (
-    fetch(`http://localhost:8080/api/category/${id}/`).then((response) => response.json())
+    fetch(`/api/category/${id}/`).then((response) => response.json())
   );
 };
 const fetchStore = async (id) => {
-  return fetch(`http://localhost:8080/api/stores/${id}`).then((response) =>
+  return fetch(`/api/stores/${id}`).then((response) =>
     response.json()
   );
 };
 const fetchTeam = async (id) => {
-  return fetch(`http://localhost:8080/api/team/${id}`).then((response) =>
+  return fetch(`/api/team/${id}`).then((response) =>
     response.json()
   );
 };
 const fetchSpotlight = async () => {
-  return fetch("http://localhost:8080/api/stores/random/").then((response) =>
+  return fetch("/api/stores/random/").then((response) =>
     response.json()
   );
 };
 const fetchAllCategories = async () => {
-  return fetch(`http://localhost:8080/api/index/`).then((response) =>
+  return fetch(`/api/index/`).then((response) =>
     response.json()
   );
 };
 const fetchCat1 = async () => {
   return (
-  fetch(`http://localhost:8080/api/category/1/`).then((response) => response.json())
+  fetch(`/api/category/1/`).then((response) => response.json())
   );
 };
 
 const addNewStore = async (store, categoryId) => {
-  return fetch(`http://localhost:8080/api/category/${categoryId}/addStore/`, 
+  return fetch(`/api/category/${categoryId}/addStore/`, 
   {
     method: "POST",
     headers: { "Content-Type": "application/json",
@@ -42,7 +42,7 @@ const addNewStore = async (store, categoryId) => {
 
 const updateStore = async (storeId, store) => {
   console.log(store);
-  return fetch(`http://localhost:8080/api/stores/${storeId}/update-store/`, 
+  return fetch(`/api/stores/${storeId}/update-store/`, 
   {
     method: "PATCH",
     headers: { "Content-Type": "application/json",
